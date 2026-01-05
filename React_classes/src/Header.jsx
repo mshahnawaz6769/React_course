62fb815d7ed9d4b4c8155079eaae4af8f8105973
mshahnawaz6769 <mshahnawaz6769@gmai.com>
2026-01-05 21:27:37 +0500

    changes

---
+++ React_classes/src/Header.jsx
@@ -0,0 +1,12 @@
+// Simple function and export concept?
+
+function Header() {
+    return (
+        <div>
+            <h1>Hello React</h1>
+            {/* <h1>Code step by step Header</h1> */}
+        </div>
+    )
+}
+
+export default Header
